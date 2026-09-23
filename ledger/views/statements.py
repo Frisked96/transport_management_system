187cb4d1c2e2aced61f1b48023b19c34a689c606
Frisked96 <satinderkaurcheema75@gmail.com>
2026-09-23 23:28:54 +0530

    feat(statements): enhance account selection logic and improve statement print layout

--- ledger/views/statements.py
+++ ledger/views/statements.py
@@ -79,7 +79,16 @@ def party_statement_pdf(request, pk):
             'balance_class': balance_class,
         })
 
-    company = CompanyAccount.objects.first()
+    account_id = request.GET.get('account') or request.GET.get('issuer')
+    if account_id:
+        company = CompanyAccount.objects.filter(pk=account_id).first()
+    else:
+        company = (
+            CompanyAccount.objects.filter(bills__party=party).first() or
+            CompanyAccount.objects.filter(financial_records__party=party).first() or
+            CompanyAccount.objects.exclude(address='').order_by('id').first() or
+            CompanyAccount.objects.order_by('id').first()
+        )
     context = {
         'party': party,
         'recipient_name': party.name,
@@ -275,7 +284,10 @@ def unified_ledger_pdf(request):
             'balance_class': balance_class,
         })
 
-    company_main = CompanyAccount.objects.first()
+    company_main = (
+        CompanyAccount.objects.exclude(address='').order_by('id').first() or
+        CompanyAccount.objects.order_by('id').first()
+    )
     context = {
         'party': company_main,
         'recipient_name': "All Company Accounts",
